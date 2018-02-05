f32a332f511ea8b86f1073c435771342ecc2670a
Marian Steinbach <marian@giantswarm.io>
2018-02-06 00:14:18 +0100

    Folie "Nächster Termin" (#1)
    * Logic and markup for calendar slide
    * Loading some events via webservice
    * Adding moment.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,6 +1,42 @@
+// true means auto playback, false means pause (click to proceed)
+var autoplay = true;
+
 // global timer for slide changes
 var slidesTimer;
 
+// global timer for events loading
+var loadEventsInterval;
+
+// global events storage
+var calendarEvents = [];
+
+// set date/time locale
+moment.locale('de');
+
+/**
+ * Function to load events from an iCal calendar
+ */
+var loadEvents = function(url, num, charset, callback) {
+  var endpoint = "https://schaufenster-service.now.sh/events/";
+  $.ajax(endpoint, {
+    data: {
+      ical_url: url,
+      num: num,
+      charset: charset
+    },
+    dataType: "json",
+    error: function(jqXHR, errorString, exception){
+      console.error("loadEvents error:", errorString);
+    },
+    success: function(data, status, xhrObject){
+      callback(data);
+    }
+  });
+};
+
+// Parse content,
+// enrich data-driven templates,
+// start dynamic playback
 $(function() {
 
   // Duration of one slide in milliseconds
@@ -16,9 +52,41 @@ $(function() {
   slides.hide();
   $(slides[currentSlide]).show();
 
+  // parse event slides
+  $(".container.event").each(function(index, element){
+    var $el = $(element);
+    var dataset = $(element).data();
+
+    // process ical
+    if (dataset.calendarUrl) {
+      loadEvents(dataset.calendarUrl, 1, dataset.calendarCharset, function(events){
+        console.log("Received event:", events[0]);
+        // TODO: make sure this event is really in the future
+        // insert event values into HTML template
+        var start = moment(events[0].start);
+        $el.find(".title").text(events[0].title);
+        $el.find(".date").text(start.format("D"));
+        $el.find(".weekday").text(start.format("dddd"));
+        $el.find(".month").text(start.format("MMMM"));
+        $el.find(".time").text(start.format("HH:mm"));
+        $el.find(".relative").text(start.fromNow());
+      });
+    }
+
+  });
+  // var loadMyEvents = function() {
+  //   var icalURL = "https://gruene-roesrath.de/termine/cal/ics/?type=150&tx_cal_controller%5Bcalendar%5D=649";
+  //   loadEvents(icalURL, 3, "utf-8", function(events){
+  //     console.log("Received these events:", events);
+  //     calendarEvents = events;
+  //   });
+  // };
+  // var daily = 1000 * 60 * 60 * 24;
+  // loadEventsInterval = window.setInterval(loadMyEvents, daily);
+  // loadMyEvents();
+
   // Function to switch slides in a rotation
   var nextSlide = function(){
-    console.log("Hiding slide", currentSlide);
     $(slides[currentSlide]).hide();
 
     // Prevent any focus markings
@@ -29,11 +97,12 @@ $(function() {
       currentSlide = 0;
     }
 
-    console.log("Showing slide", currentSlide);
     $(slides[currentSlide]).show();
 
     window.clearTimeout(slidesTimer);
-    slidesTimer = window.setTimeout(nextSlide, slideDuration);
+    if (autoplay) {
+      slidesTimer = window.setTimeout(nextSlide, slideDuration);
+    }
   };
 
   // Click handler for manual skipping (good for testing)
@@ -42,5 +111,7 @@ $(function() {
   });
 
   // Start the rotation
-  slidesTimer = window.setTimeout(nextSlide, slideDuration);
+  if (autoplay) {
+    slidesTimer = window.setTimeout(nextSlide, slideDuration);
+  }
 });
